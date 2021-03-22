8f4d3e691619c03213b74e79ffc7cd0da3ef9ce7
FherPie <panaonchoatu@hotmail.com>
2021-03-22 15:13:45 -0500

    First Commit

---
+++ src/main/java/com/escuela2/escuela2/Escuela2Application.java
@@ -0,0 +1,13 @@
+package com.escuela2.escuela2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Escuela2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Escuela2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/escuela2/escuela2/Escuela2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuela2.escuela2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Escuela2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
